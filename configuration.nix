07a1afd903dbd100b446bf8d837a1452be739660
merzeria <84082125+merzeria@users.noreply.github.com>
2026-02-17 20:41:56 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -42,8 +42,24 @@
 
   # Select internationalisation properties.
 
-  i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
     console.keyMap = "dk";
+  # Extra language settings
+  i18n.extraLocaleSettings = {
+   LC_ALL = "en_GB.UTF-8";
+   LC_CTYPE = "da_DK.UTF8";
+   LC_ADDRESS = "da_DK.UTF-8";
+   LC_IDENTIFICATION = "da_DK.UTF-8";
+   LC_MEASUREMENT = "da_DK.UTF-8";
+   LC_MESSAGES = "da_DK.UTF-8";
+   LC_MONETARY = "da_DK.UTF-8";
+   LC_NAME = "da_DK.UTF-8";
+   LC_NUMERIC = "da_DK.UTF-8";
+   LC_PAPER = "da_DK.UTF-8";
+   LC_TELEPHONE = "da_DK.UTF-8";
+   LC_TIME = "da_DK.UTF-8";
+   LC_COLLATE = "da_DK.UTF-8";
+  };
 
   # enable OpenGL
 	hardware.graphics = {
@@ -121,6 +137,7 @@
     pkgs.protonplus
     pkgs.protontricks
     pkgs.alsa-utils
+    pkgs.thunderbird-latest-unwrapped
   ];
 
   # List packages installed in system profile.
