6e5276ce63ea22d92a57ff3cf8208986062b6f92
merzeria <84082125+merzeria@users.noreply.github.com>
2026-02-18 07:10:04 +0100

    Update filesystem.nix

--- filesystem.nix
+++ filesystem.nix
@@ -11,11 +11,8 @@
  fileSystems."/run/media/simon/d213c7a5-7c19-4001-947f-d849737cef60" = {
    device = "/dev/nvme0n1p1";
    fsType = "ext4";
-   options = [ # If you don't have this options attribute, it'll default to "defaults"
-     # boot options for fstab. Search up fstab mount options you can use
-     "users" # Allows any user to mount and unmount
-     "nofail" # Prevent system from failing if this drive doesn't mount
-
+   options = [ 
+    "nofail" # Prevent system from failing if this drive doesn't mount
    ];
  };
 }
