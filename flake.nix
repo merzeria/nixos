525eebc8434197ea61cca5952606b6110fc3ad3f
merzeria <stwhal@protonmail.com>
2026-02-20 18:13:45 +0100

    Edit flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,55 +10,34 @@
     plasma-manager = {
       url = "github:nix-community/plasma-manager";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, plasma-manager, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        username = "simon";          # your Linux user name
-      in {
-        # ---------------------------------------------------------
-        # Two complete host configurations – just change the suffix
-        # ---------------------------------------------------------
+  outputs = { self, nixpkgs, home-manager, plasma-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      username = "simon";
 
-        nixosConfigurations.nixos-nordic = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            # 1️⃣ Hardware detection (generated file)
-            ./hardware-configuration.nix
-
-            # 2️⃣ System‑wide options (your old configuration.nix content)
-            ./modules/system.nix
-
-            # 3️⃣ Plasma‑manager core module
-            plasma-manager.nixosModules.plasma-manager
-
-            # 4️⃣ Theme selector – Nordic
-            (import ./modules/plasma.nix { inherit pkgs; themeName = "nordic"; })
-
-            # 5️⃣ Home‑Manager integration
-            ({ config, pkgs, ... }: {
-              home-manager.useUserPackages = true;
-              home-manager.users.${username} = import ./modules/home.nix;
-            })
-          ];
-        };
-
-        nixosConfigurations.nixos-sweet = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hardware-configuration.nix
-            ./modules/system.nix
-            plasma-manager.nixosModules.plasma-manager
-            (import ./modules/plasma.nix { inherit pkgs; themeName = "sweet"; })
-            ({ config, pkgs, ... }: {
-              home-manager.useUserPackages = true;
-              home-manager.users.${username} = import ./modules/home.nix;
-            })
-          ];
-        };
-      });
+      mkHost = themeName: nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hardware-configuration.nix
+          ./modules/system.nix
+          home-manager.nixosModules.home-manager # Critical: Loads HM module
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs themeName; };
+            home-manager.users.${username} = import ./modules/home.nix;
+          }
+        ];
+      };
+    in {
+      nixosConfigurations = {
+        sweet  = mkHost "sweet";
+        nordic = mkHost "nordic";
+      };
+    };
 }
