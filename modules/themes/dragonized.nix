cda90be6efc79aeb6ae65909ecb18707ebd7206b
Simon Halberg <stwhal@protonmail.com>
2026-02-28 13:47:09 +0100

    Update: 2026-02-28

--- modules/themes/dragonized.nix
+++ modules/themes/dragonized.nix
@@ -26,16 +26,14 @@
     configFile = {
       "kdeglobals"."General"."widgetStyle" = "kvantum";
       "kvantumrc"."General"."theme"        = "Sweet-transparent-toolbar";
-
-      # Use the Sweet Konsole profile (same palette as dragonized)
       "konsolerc"."Desktop Entry"."DefaultProfile" = "Sweet.profile";
 
-      # KWin Effects
+      # KWin Effects - Plasma 6 key names
       "kwinrc"."Plugins" = {
-        blurEnabled                      = true;
-        kwin4_effect_translucencyEnabled = true;
-        slidebackEnabled                 = true;
-        minimizeanimationEnabled         = true;
+        blurEnabled              = true;
+        translucencyEnabled      = true;  # Plasma 6 (was kwin4_effect_translucencyEnabled)
+        slidebackEnabled         = true;
+        minimizeanimationEnabled = true;
       };
 
       "kwinrc"."Effect-blur" = {
@@ -44,11 +42,11 @@
       };
 
       "kwinrc"."Effect-translucency" = {
-        Active    = 100;
-        Inactive  = 88;
+        Active     = 100;
+        Inactive   = 88;
         MoveResize = 80;
-        Dialogs   = 100;
-        Desktop   = 100;
+        Dialogs    = 100;
+        Desktop    = 100;
       };
 
       "kwinrc"."Compositing" = {
@@ -114,6 +112,73 @@
       }
     ];
   };
+
+  xdg.configFile."konsole/Sweet.colorscheme".text = ''
+    [General]
+    Description=Sweet
+    Opacity=0.85
+    Wallpaper=
+
+    [Background]
+    Color=#1e1d2f
+    [BackgroundIntense]
+    Color=#2a2a3d
+
+    [Foreground]
+    Color=#e0def4
+    [ForegroundIntense]
+    Color=#ffffff
+
+    [Color0]
+    Color=#1e1d2f
+    [Color0Intense]
+    Color=#3b3a52
+
+    [Color1]
+    Color=#f05e97
+    [Color1Intense]
+    Color=#f07da8
+
+    [Color2]
+    Color=#5eff6c
+    [Color2Intense]
+    Color=#80ff8c
+
+    [Color3]
+    Color=#f3c173
+    [Color3Intense]
+    Color=#f5ce8e
+
+    [Color4]
+    Color=#7e9ef5
+    [Color4Intense]
+    Color=#9ab0f5
+
+    [Color5]
+    Color=#b072d1
+    [Color5Intense]
+    Color=#c084e0
+
+    [Color6]
+    Color=#70e6d0
+    [Color6Intense]
+    Color=#8aeedd
+
+    [Color7]
+    Color=#e0def4
+    [Color7Intense]
+    Color=#ffffff
+  '';
+
+  xdg.configFile."konsole/Sweet.profile".text = ''
+    [Appearance]
+    ColorScheme=Sweet
+    Font=JetBrainsMono Nerd Font,10,-1,5,50,0,0,0,0,0
+
+    [General]
+    Name=Sweet
+    Parent=FALLBACK/
+  '';
 
   home.packages = with pkgs; [
     sweet-nova
